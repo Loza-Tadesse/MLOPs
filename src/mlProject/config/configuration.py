0cb5bdc5912cac4664318fe42deeec39aa4995f5
Loza-Tadesse <lozamulugetatadesse@gmail.com>
2025-03-11 13:30:00 -0400

    refactor: Update configuration manager for new components

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -5,6 +5,9 @@ from mlProject.entity.config_entity import (DataValidationConfig)
 from mlProject.entity.config_entity import (DataTransformationConfig)
 from mlProject.entity.config_entity import (ModelTrainerConfig)
 from mlProject.entity.config_entity import (ModelEvaluationConfig)
+from mlProject.entity.config_entity import (DeepModelTrainerConfig)
+from mlProject.entity.config_entity import (DeepModelEvaluationConfig)
+from mlProject.entity.config_entity import (FeatureEngineeringConfig)
 
 
 class ConfigurationManager:
@@ -20,6 +23,10 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
+    def get_cryptocurrencies(self) -> list:
+        """Get list of cryptocurrencies from configuration"""
+        return self.config.cryptocurrencies
+
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -27,7 +34,7 @@ class ConfigurationManager:
 
         data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
-            source_URL=config.source_URL,
+            source_url=config.source_url,
             local_data_file=config.local_data_file,
             unzip_dir=config.unzip_dir
         )
@@ -62,7 +69,7 @@ class ConfigurationManager:
 
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
-        params = self.params.ElasticNet
+        params = self.params.RandomForest
         schema = self.schema.TARGET_COLUMN
 
         create_directories([config.root_dir])
@@ -72,8 +79,9 @@ class ConfigurationManager:
             train_data_path=config.train_data_path,
             test_data_path=config.test_data_path,
             model_name=config.model_name,
-            alpha=params.alpha,
-            l1_ratio=params.l1_ratio,
+            n_estimators=params.n_estimators,
+            max_depth=params.max_depth,
+            min_samples_split=params.min_samples_split,
             target_column=schema.name
 
         )
@@ -82,7 +90,7 @@ class ConfigurationManager:
 
     def get_model_evaluation_config(self) -> ModelEvaluationConfig:
         config = self.config.model_evaluation
-        params = self.params.ElasticNet
+        params = self.params.RandomForest
         schema = self.schema.TARGET_COLUMN
 
         create_directories([config.root_dir])
@@ -99,3 +107,61 @@ class ConfigurationManager:
         )
 
         return model_evaluation_config
+
+    def get_deep_model_trainer_config(self) -> DeepModelTrainerConfig:
+        config = self.config.deep_model_trainer
+        params = self.params.DeepLearning
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        deep_model_trainer_config = DeepModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path=config.train_data_path,
+            test_data_path=config.test_data_path,
+            model_name=config.model_name,
+            hidden_layers=params.hidden_layers,
+            dropout_rate=params.dropout_rate,
+            learning_rate=params.learning_rate,
+            batch_size=params.batch_size,
+            epochs=params.epochs,
+            early_stopping_patience=params.early_stopping_patience,
+            target_column=schema.name
+        )
+
+        return deep_model_trainer_config
+
+    def get_deep_model_evaluation_config(self) -> DeepModelEvaluationConfig:
+        config = self.config.deep_model_evaluation
+        params = self.params.DeepLearning
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        deep_model_evaluation_config = DeepModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path=config.model_path,
+            scaler_path=config.scaler_path,
+            model_config_path=config.model_config_path,
+            all_params=params,
+            metric_file_name=config.metric_file_name,
+            target_column=schema.name,
+            mlflow_uri="https://dagshub.com/Loza-Tadesse/MLOPs.mlflow",
+        )
+
+        return deep_model_evaluation_config
+
+    def get_feature_engineering_config(self) -> FeatureEngineeringConfig:
+        config = self.config.feature_engineering
+
+        create_directories([config.root_dir])
+
+        feature_engineering_config = FeatureEngineeringConfig(
+            root_dir=config.root_dir,
+            metadata_path=config.metadata_path,
+            enable_validation=config.enable_validation,
+            drift_threshold=config.drift_threshold
+        )
+
+        return feature_engineering_config
